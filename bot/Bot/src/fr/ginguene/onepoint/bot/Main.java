28688adcb6a3ff6e2ff6728c680aeb88d8d3eaf4
ginguene <ginguene@memnon>
2017-09-21 14:37:22 +0200

    test

--- bot/Bot/src/fr/ginguene/onepoint/bot/Main.java
+++ bot/Bot/src/fr/ginguene/onepoint/bot/Main.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package main.java.fr.ginguene.onepoint.bot;
+package fr.ginguene.onepoint.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
